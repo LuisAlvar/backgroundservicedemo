cedd8b331c8d95717b9b894415a1921427fb10f1
Luis Alvarez <alvarez.en.luis@gmail.com>
2022-07-27 01:32:26 -0700

    ready to publish application

--- BackgroundServiceDemo/Program.cs
+++ BackgroundServiceDemo/Program.cs
@@ -1,9 +1,23 @@
+using App.WindowsService;
 using BackgroundServiceDemo;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 
 IHost host = Host.CreateDefaultBuilder(args)
+    // UseWindowsService extension method configures the app to work as a Windows Service. 
+    // The host service is registered for dependency injection. 
+    .UseWindowsService(options => {
+        options.ServiceName = ".Net Joke Service";
+    })
     .ConfigureServices(services =>
     {
+        LoggerProviderOptions
+            .RegisterProviderOptions<EventLogSettings, EventLogLoggerProvider>(services);
+        services.AddSingleton<JokeService>();
         services.AddHostedService<Worker>();
+    })
+    .ConfigureLogging((context, logging) => {
+        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
     })
     .Build();
 
